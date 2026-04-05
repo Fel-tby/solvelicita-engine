2155a77af59f26446829e302322d4a5af346c39b
Luis Felipe <thebaypaypal@gmail.com>
2026-04-05 02:41:01 -0300

    chore: front analytics fix

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import 'leaflet/dist/leaflet.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   )
 }
